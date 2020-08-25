8f6c7c452a73dddbe3a176a8300a8f22eb607851
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-08-25 12:18:43 +0530

    modified files

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 //Importing components
 
-import Login from './components/login';
+import Login from './components/login'
 import Signup from './components/signup';
 import Recruit from './components/recruiter';
 
 const Routes = () =>{
     return(
     <Switch>
-        <Route exact path="/" component={Login}/>
+        <Route exact path="/" component={Login} />
         <Route    path="/signup" component={Signup}/>
         <Route   path="/recruiter-signup" component={Recruit}/>
     </Switch>
