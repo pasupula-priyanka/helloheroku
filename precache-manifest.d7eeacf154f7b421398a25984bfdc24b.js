6b85039029e68c8aafd16a83c2ecfb687aa69124
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-08-25 12:26:25 +0530

    Updates

---
+++ precache-manifest.d7eeacf154f7b421398a25984bfdc24b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70da9413b2941dc17d3d57a122600d43",
+    "url": "/helloheroku/index.html"
+  },
+  {
+    "revision": "4ff656c7cd1a09332475",
+    "url": "/helloheroku/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e09e8acbeade46fbe746",
+    "url": "/helloheroku/static/css/main.79ed88ca.chunk.css"
+  },
+  {
+    "revision": "4ff656c7cd1a09332475",
+    "url": "/helloheroku/static/js/2.422ee966.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/helloheroku/static/js/2.422ee966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09e8acbeade46fbe746",
+    "url": "/helloheroku/static/js/main.5e401bb4.chunk.js"
+  },
+  {
+    "revision": "8a70ab12716c44a9417d",
+    "url": "/helloheroku/static/js/runtime-main.54756f11.js"
+  }
+]);
